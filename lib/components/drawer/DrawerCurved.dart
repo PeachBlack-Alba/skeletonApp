f44b1dd751b244102430b491b3df1d94bd6d9e55
PeachBlack-Alba <peachblackweb@gmail.com>
2021-02-02 11:00:42 +0100

    push

--- lib/components/drawer/DrawerCurved.dart
+++ lib/components/drawer/DrawerCurved.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 import 'package:skeleton_app/profile/components/DrawerItem.dart';
+import 'package:skeleton_app/styles/Colors.dart';
 import 'package:thebase_app/ui/routing/routes.dart';
 import 'package:thebase_app/ui/screens/profile/components/BottomSheetShape.dart';
 import 'package:thebase_app/ui/screens/profile/components/DrawerItem.dart';
